3963901f8e633e831cefcc12da935827dea0a1a6
liz rush <liz@lizmrush.com>
2014-01-06 13:29:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mediatracker::Application.config.secret_key_base = 'a4c4240f3bd274fd6f712e199f013118c2c5f2513e2d5b54a0f9e8d0ab7ad39e1e5e45d3dedd5b3917eda6bbe34e258be1b5de9c1494ff52549ddb993ab40502'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mediatracker::Application.config.session_store :cookie_store, key: '_mediatracker_session'
